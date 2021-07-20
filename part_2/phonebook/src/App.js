fc14944c2901482d5c435f0b5d6532e41e053c69
L. Chiavelli <ludovico.chiavelli@outlook.com>
2021-07-20 11:29:15 +0300

    Exercise 2.9 search field implemented

--- part_2/phonebook/src/App.js
+++ part_2/phonebook/src/App.js
@@ -1,51 +1,65 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber ] = useState('')
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [newSearch, setNewSearch] = useState("");
 
   const addPerson = (event) => {
-    event.preventDefault()
-    let names = []
-    for(let i = 0; i < persons.length; i++) {
-      names.push(persons[i].name)
+    event.preventDefault();
+    let names = [];
+    for (let i = 0; i < persons.length; i++) {
+      names.push(persons[i].name);
     }
 
-    if(Object.values(names).includes(newName)) {
-      alert(`${newName} is already in the phonebook`)
-      setNewName('')
-      setNewNumber('')
+    if (Object.values(names).includes(newName)) {
+      alert(`${newName} is already in the phonebook`);
+      setNewName("");
+      setNewNumber("");
     } else {
       const personObject = {
         name: newName,
-        number: newNumber
-      }
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
+        number: newNumber,
+      };
+      setPersons(persons.concat(personObject));
+      setNewName("");
+      setNewNumber("");
     }
-  }
+  };
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+    setNewNumber(event.target.value);
+  };
+
+  const handleSearchChange = (event) => {
+    setNewSearch(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with{" "}
+        <input value={newSearch} onChange={handleSearchChange} />
+      </div>
+      <h2>Add a new person</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange}/>
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          phone number:  <input value={newNumber} onChange={handleNumberChange}/>
+          phone number:{" "}
+          <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -53,10 +67,24 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+        {persons
+          .filter((person) => {
+            if (newSearch === "") {
+              return person;
+            } else if (
+              person.name.toLowerCase().includes(newSearch.toLowerCase())
+            ) {
+              return person;
+            }
+          })
+          .map((person) => (
+            <p key={person.name}>
+              {person.name} {person.number}
+            </p>
+          ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
