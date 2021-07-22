992e0528b3bb7294b10562011b422df07baf415b
L. Chiavelli <ludovico.chiavelli@outlook.com>
2021-07-22 08:58:44 +0300

    Exercise 2.11, separated app into components

--- part_2/phonebook/src/App.js
+++ part_2/phonebook/src/App.js
@@ -1,16 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newSearch, setNewSearch] = useState("");
@@ -26,6 +22,13 @@ const App = () => {
   const handleSearchChange = (event) => {
     setNewSearch(event.target.value);
   };
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log(response.data);
+      setPersons(response.data);
+    });
+  }, []);
 
   return (
     <div>
