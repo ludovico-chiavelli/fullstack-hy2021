5d303d1f9344df7cd5b72597b84c0ded0b89ce3a
L. Chiavelli <ludovico.chiavelli@outlook.com>
2021-09-28 12:00:58 +0300

    Modified app for production

--- part_2/phonebook/src/services/persons.js
+++ part_2/phonebook/src/services/persons.js
@@ -1,20 +1,20 @@
-import axios from "axios"
-const baseURL = "http://localhost:3001/persons"
+import axios from "axios";
+const baseURL = "/api/persons";
 
 const getAll = () => {
-    return axios.get(baseURL)
-}
+  return axios.get(baseURL);
+};
 
-const create = newObject => {
-    return axios.post(baseURL, newObject)
-}
+const create = (newObject) => {
+  return axios.post(baseURL, newObject);
+};
 
 const update = (id, newObject) => {
-    return axios.put(`${baseURL}/${id}`, newObject)
-}
+  return axios.put(`${baseURL}/${id}`, newObject);
+};
 
 const remove = (id) => {
-    return axios.delete(`${baseURL}/${id}`)
-}
+  return axios.delete(`${baseURL}/${id}`);
+};
 
-export default { getAll, create, update, remove}
+export default { getAll, create, update, remove };
