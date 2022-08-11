36a177a808ce228bb59930f16d5bbfd477c6f65c
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-08-11 15:54:39 +0100

    exercise 3.22. Install ESLint and fix errors

---
+++ part_2/phonebook/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    'env': {
+        'javascript': true,
+        'es2021': true,
+        'node': true
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 12
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ]
+    }
+}
